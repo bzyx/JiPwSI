8d03b3b38f192dd71a9cb36f0eeb4f6808284dc8
Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
2011-11-15 20:39:54 +0100

    Added new type of Excepiton for new menu.

---
+++ src/pl/polsl/flota/exceptions/MenuItemNotFound.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package pl.polsl.flota.exceptions;
+
+/**
+ * @author bzyx
+ *
+ */
+public class MenuItemNotFound extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public MenuItemNotFound() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public MenuItemNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
